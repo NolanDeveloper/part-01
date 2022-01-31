d40c11579d5b7aecd3a8cca5005a6e4ce3517b0c
Denis M <Nolane16@gmail.com>
2022-01-31 20:20:33 +0300

    Initial commit

---
+++ 1.8/step-12.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    /* ... */
+
+    char c = '\0';
+    while (cin.get(c)) {
+        /* ... */                    
+    }
+
+	return 0;
+}
